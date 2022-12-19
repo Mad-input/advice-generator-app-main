7288fc8fc4faf480a405a04aa8b93ce8ccbb9771
Miguel <macc07112005c80@gmail.com>
2022-12-19 18:08:39 -0500

    animation

--- js/main.js
+++ js/main.js
@@ -6,12 +6,12 @@ const content = document.getElementById("content");
 
 let newAdvice = advice[Math.round(Math.random() * advice.length)];
 title.innerText = `ADVICE #${newAdvice.id}`;
-content.innerText = newAdvice.content;
+content.innerText = `"${newAdvice.content}"`;
 
 const btnGenerate = document.getElementById("btnGenerate");
 
 btnGenerate.addEventListener("click", () => {
   let newAdvice = advice[Math.round(Math.random() * advice.length)];
   title.innerText = `ADVICE #${newAdvice.id}`;
-  content.innerText = newAdvice.content;
+  content.innerText = `"${newAdvice.content}"`;
 });
